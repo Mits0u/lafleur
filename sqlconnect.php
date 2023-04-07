888be8a3a37a19c85ed0e0e9f728e2cd2ab6b1a7
stpoltron <stpoltron@stpbb.org>
2023-04-07 10:49:27 +0200

    ajout du script de connexion a la base de donnée

--- sqlconnect.php
+++ sqlconnect.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Connection à la base de données
+ * Connexion à la base de données
  *
  * PHP Version 7
  *
- * @category Connection
- * @package  Connection
+ * @category Connexion
+ * @package  Connexion
  * @author   Poltron Steven <stpoltron@stpbb.org>
  * @license  https://www.php.net/license/3_01.txt PHP License 3.01
  * @link     http://www.stpbb.org
@@ -16,12 +16,12 @@ try {
 
     // le lien pour se connecter a la base de données mysql
     $dns = 'mysql:host=localhost;dbname=lafleuriste';
-    // le  pour se connecter
-    $mail = $_SESSION['mail'];
+    // le nom d'utilisateur pour se connecter
+    $utilisateur = 'root';
     // le mot de passe de l'utilisateur pour se connecter
-    $password = $_SESSION['password'];
+    $password = '';
     // on se connecte à MySQL
-    $connection = new PDO($dns, $mail, $password);
+    $connection = new PDO($dns, $utilisateur, $password);
     // on définit l'encondage des caractères en utf8
     $connection->query("SET NAMES utf8");
 } catch ( Exception $e ) { // en cas d'erreur
