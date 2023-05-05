e189b49c7ef604564bfac698670300e7e8ccb614
stpoltron <stpoltron@stpbb.org>
2023-05-05 09:06:54 +0200

    fix des probleme de modification et d'insertion

---
+++ inscription.php
@@ -0,0 +1,48 @@
+<?php 
+    require_once 'sqlconnect.php'; // On inclu la connexion à la bdd
+
+    // creation du formulaire
+    echo '<form action="inscription.php" method="post">
+        <p>
+            <label for="nom">Nom</label> : <input type="text" name="nom" id="nom" /><br />
+            <label for="prenom">Prenom</label> : <input type="prenom" name="prenom" id="prenom" /><br />
+            <label for="adresse">Adresse</label> : <input type="text" name="adresse" id="adresse" /><br />
+            <label for="pass1">Mot de passe</label> : <input type="password" name="pass1" id="pass1" /><br />
+            <label for="pass2">Confirmer le mot de passe</label> : <input type="password" name="pass2" id="pass2" /><br />
+            <label for="mail">Email</label> : <input type="email" name="mail" id="mail" /><br />
+            <label for="tel">Telephone</label> : <input type="text" name="tel" id="tel" /><br />
+            <input type="submit" value="Inscription" />
+        </p>
+    </form>';
+
+    // si le formulaire est envoyé
+
+    if (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['adresse']) && isset($_POST['pass1']) && isset($_POST['pass2']) && isset($_POST['mail']) && isset($_POST['tel'])) {
+        // on vérifie que les deux mots de passe sont identiques
+        if ($_POST['pass1'] == $_POST['pass2']) {
+            // on vérifie que le mail n'est pas déjà utilisé
+            $sql = "SELECT * FROM utilisateurs WHERE mail = :mail";
+            $results = $connection->prepare($sql);
+            $results->bindParam(':mail', $_POST['mail']);
+            $results->execute();
+            $array = $results->fetchAll();
+            if (count($array) == 0) {
+                // on ajoute l'utilisateur à la base de données
+                $sql = "INSERT INTO utilisateurs (mail, nom, prenom, adresse, tel, password) VALUES (:mail, :nom, :prenom, :adresse, :tel, :password)";
+                $sql = $connection->prepare($sql);
+                $sql->bindParam(':mail', $_POST['mail']);
+                $sql->bindParam(':nom', $_POST['nom']);
+                $sql->bindParam(':prenom', $_POST['prenom']);
+                $sql->bindParam(':adresse', $_POST['adresse']);
+                $sql->bindParam(':tel', $_POST['tel']);
+                $sql->bindParam(':password', $_POST['pass1']);
+                $sql->execute();
+                echo '<h1>' . 'Inscription réussie' . '</h1>';
+            } else {
+                echo "Erreur d'inscription";
+            }
+        } else {
+            echo "Erreur d'inscription";
+        }
+    }
+?>
